e26319cba58925a8b5fe62733a3309062d549640
dev-ankit-0067 <ankit.sh0067@gmail.com>
2026-03-12 19:40:39 +0000

    db persists

--- data/small_ai.py
+++ data/small_ai.py
@@ -2,7 +2,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
+import os
 
 
 def ask_question(question):
@@ -21,47 +22,57 @@ Question:
 {question}
 """
 
-    response = llm.invoke(prompt)
-
-    return response
-
+    for answer in llm.stream(prompt):
+        print(answer, end="")
 
 
-loader = PyPDFLoader('./Problem Management Process.pdf')
-docs = loader.load()
 
-print("Loaded document with", len(docs), "pages.")
 
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=500,
-    chunk_overlap=100
-)
+if not os.path.exists("./vectordb"):
+    loader = PyPDFLoader('./Problem Management Process.pdf')
+    docs = loader.load()
 
-print("Splitting document into chunks...")
-chunks = splitter.split_documents(docs)
+    print("Loaded document with", len(docs), "pages.")
 
-print("Number of chunks:", len(chunks))
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
 
-print("Creating embeddings...")
-embedding = HuggingFaceEmbeddings(
-  model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+    print("Splitting document into chunks...")
+    chunks = splitter.split_documents(docs)
 
-print("Creating vector database...")
-vectordb = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding,
-    persist_directory="./vectordb"
-)
+    print("Number of chunks:", len(chunks))
 
-print("Vector database created.")
+    print("Creating embeddings...")
+    embedding = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    print("Creating vector database...")
+    vectordb = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding,
+        persist_directory="./vectordb"
+    )
+
+    vectordb.persist()
+    print("Vector database created and persisted.")
+else:
+    print("Loading existing vector database...")
+    vectordb = Chroma(
+        embedding_function=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2"),
+        persist_directory="./vectordb"
+    )
+    print("Vector database loaded.")
 
 retriever = vectordb.as_retriever()
 
-llm = Ollama(model="phi3")
+llm = OllamaLLM(model="phi3")
+
 
 print("Asking question...")
 
-answer = ask_question("What is this document about?")
+ask_question("What is the role of Queue Manager?")
 
-print("\nAnswer:", answer)
+
